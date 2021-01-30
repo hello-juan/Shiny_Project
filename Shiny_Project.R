8143c678c9ca9889630fe7605ff0aa59f52247ec
Juan Vasquez <juanrvasquezjr@gmail.com>
2021-01-30 14:04:06 -0500

    Updated R Script with Categorical Barplots

--- Shiny_Project.R
+++ Shiny_Project.R
@@ -6,10 +6,38 @@ library(lubridate)
 
 Permits <- read.csv("Film_Permit.csv")
 
-Permits %>% mutate(Start.Date = as.Date(StartDateTime,"%m/%d/%Y"))
- 
-Permits %>% mutate(End.Date = as.Date(EndDateTime,"%m/%d/%Y"))
+Permits <- Permits%>%select(., -15, -16, -18)
+
+Permits <- Permits %>% mutate(Start.Date.Time = mdy_hms(StartDateTime, tz = Sys.timezone()))
+
+Permits <- Permits %>% mutate(End.Date.Time = mdy_hms(EndDateTime, tz = Sys.timezone()))
+
+Permits <- Permits %>% mutate(Difference_In_Time = End.Date.Time - Start.Date.Time, )
+
+Permits <- Permits %>% mutate(Start.Month = month(Permits$Start.Date.Time), Start.Year = year(Start.Date.Time), End.Month = month(End.Date.Time), End.Year = year(End.Date.Time))
+
+Permits <- Permits %>% mutate(Start.Hour = hour(Start.Date.Time), End.Hour = hour(End.Date.Time))
+
+Permits %>%
+  ggplot() + geom_bar(aes(Permits$Borough, fill = Permits$Category), position = "dodge")
+
+Permits %>%
+  ggplot() + geom_bar(aes(Permits$Borough, fill = Permits$SubCategory), position = "dodge")
+
+Permits %>%
+  ggplot() + geom_bar(aes(Permits$Borough))
 
 Permits%>%
-  group_by(Borough, EventType)%>%
+  ggplot() + geom_bar(aes(Permits$Category))
+
+Permits %>%
+  ggplot() + geom_bar(aes(Permits$SubCategoryName))
+
+Permits%>%
+  ggplot()
+  
+
+View(Permits) 
+
+Permits%>%
   ggplot() + geom_histogram(aes(EventType))
